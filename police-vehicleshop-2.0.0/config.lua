f1bd75da8df475890ef2eaa0a05963465a24355a
dacca kareem <89742984+daccakareem@users.noreply.github.com>
2022-02-07 09:24:50 +0200

    script update

---
+++ police-vehicleshop-2.0.0/config.lua
@@ -0,0 +1,11 @@
+Config = Config or {}
+
+Config.vehicle = {
+    npolvic = 'npolvic',
+    npolchar = 'npolchar'
+}
+
+Config.cost = {
+    npolvic = 2000,
+    npolchar = 5000
+}
---
+++ police-vehicleshop-2.0.0/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'kareem dacca github = daccakareem'
+
+client_scripts {
+	'client/main.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'server/main.lua',
+	'config.lua'
+}
